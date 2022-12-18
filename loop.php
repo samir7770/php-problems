dd108dcf183dac52c20c61cc478af25f56e0d118
Mashiuzzaman Samir <mashiuzzamansamir@gmail.com>
2022-12-18 14:42:52 +0600

    Added

--- loop.php
+++ loop.php
@@ -3,6 +3,22 @@
 //     echo $i.":". (100 - $i).PHP_EOL;
 // }
 
-for ($i=0, $j=1; $i<=10; $i++, $j++){
-    echo $i + $j . PHP_EOL;
+// for ($i=0, $j=1; $i<=10; $i++, $j++){
+//     echo $i + $j . PHP_EOL;
+// }
+
+// Factorial of 10
+
+echo PHP_EOL;
+for ($i= 6, $fac = 1; $i>0; $i--){
+    $fac *= $i;
 }
+// echo $fac;
+
+
+for ($i = 0; $i<10; $i++){
+    $fibo = $i;
+    $new = $fibo + $i;
+    echo $new.PHP_EOL;
+}
+
