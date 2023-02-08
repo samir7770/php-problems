4d5925537e856a6fdd20e18a7c86ba02f3c72ec8
Mashiuzzaman Samir <mashiuzzamansamir@gmail.com>
2023-02-09 00:32:50 +0600

    Problems added

--- function.php
+++ function.php
@@ -1,16 +1,72 @@
 <?php
 
-function myFunc( int $min, $max ) {
-    $total = 0;
-    for ( $i = $min; $i <= $max; $i++ ) {
-        $total += $i;
-    }
-    return $total;
-}
+// function myFunc( int $min, $max ) {
+//     $total = 0;
+//     for ( $i = $min; $i <= $max; $i++ ) {
+//         $total += $i;
+//     }
+//     return $total;
+// }
 
-// echo myFunc(2, 5);
+// // echo myFunc(2, 5);
 
-function check( int $a, int $b, ...$c ) {
-    return $a + $b . $c[0] . $c[1] . $c[2] . $c[3] . $c[4];
-}
-echo check( 2, 3, 4, 5, 6, 7, 8 );
+// function check( int $a, int $b, ...$c ) {
+//     return $a + $b . $c[0] . $c[1] . $c[2] . $c[3] . $c[4];
+// }
+// echo check( 2, 3, 4, 5, 6, 7, 8 );
+
+// ===========================
+// PROBLEMS
+// ===========================
+
+// Write a PHP function that takes a string as an argument and returns a new string with the following transformations applied:
+
+// All letters should be converted to lowercase
+// All vowels should be removed
+// All remaining letters should be sorted in alphabetical order
+// For example, if the input string is "Hello World", the output string should be "hllwrd".
+
+// function transform( $str ) {
+//     $strToLower = strtolower( $str );
+
+//     if ( $strToLower ) {
+//         return $strToLower;
+//     }
+
+// }
+
+// echo transform( "Ashur" );
+
+// ===========================
+// PROBLEMS
+// ===========================
+
+// একটা function দেওয়া হবে called “isLGSeven()”. এটা ইনপুট হিসেবে একটা number নিবে।
+
+// task: তোমাকে ইনপুট ভ্যালু এবং ৭ এর মধ্যে absolute পার্থক্য বের করতে হবে। যদি এই পার্থক্য ৭ এর চেয়ে ছোট হয়, তখন তোমাকে return করতে হবে সেই ইনপুট এবং ৭ এর মধ্যে পার্থক্য। নাহলে তোমাকে return করতে হবে double of their absolute difference.
+// =================
+// Sample Input & Output:-
+
+// Input: 6  Output: -1
+// Input: -15  Output: 44
+// Input: 13 Output: 6
+
+// function isLGSeven( $num ) {
+//     $diff = abs( $num - 7 );
+//     if ( $diff < 7 ) {
+//         return $num - 7;
+//     } else {
+//         return $diff * 2;
+//     }
+// }
+
+// echo isLGSeven( -15 );
+
+// echo PHP_EOL;
+
+$isLGSeven = fn( $n ) => ( abs( $n - 7 ) < 7 ) ? $n - 7 : abs( $n - 7 ) * 2;
+
+echo $isLGSeven( 13 );
+echo PHP_EOL;
+$isSeven = fn( $v ) => 7 >= abs( $v ) ? $v - 7 : ( 7 >= abs( 7 - $v ) ? abs( $v - 7 ) : abs( 7 - $v ) * 2 );
+echo $isSeven( 13 );
--- loop.php
+++ loop.php
@@ -9,14 +9,49 @@
 
 // Factorial of 10
 
-echo PHP_EOL;
-for ( $i = 6, $fac = 1; $i > 0; $i-- ) {
-    $fac *= $i;
-}
+// echo PHP_EOL;
+// for ( $i = 6, $fac = 1; $i > 0; $i-- ) {
+//     $fac *= $i;
+// }
 // echo $fac;
 
-for ( $i = 0; $i < 10; $i++ ) {
-    $fibo = $i;
-    $new = $fibo + $i;
-    echo $new . PHP_EOL;
+// for ( $i = 0; $i < 10; $i++ ) {
+//     $fibo = $i;
+//     $new = $fibo + $i;
+//     echo $new . PHP_EOL;
+// }
+
+// ===========================
+// PROBLEMS
+// ===========================
+
+// Create a script that displays 1-2-3-4-5-6-7-8-9-10 on one line. There will be no hyphen(-) at starting and ending position.
+
+// for ( $i = 1; $i <= 10; $i++ ) {
+//     echo $i;
+//     if ( $i == 10 ) {
+//         break;
+//     }
+//     echo " - ";
+// }
+
+// ===========================
+// PROBLEMS
+// ===========================
+
+// Create a script using a for loop to add all the integers between 0 and 30 and display the total
+
+// $total = 0;
+// for ( $i = 1; $i <= 30; $i++ ) {
+//     $total += $i;
+// }
+
+// echo $total;
+
+for ( $i = 1; $i <= 10; $i++ ) {
+    echo $i;
+    if ( $i == 10 ) {
+        break;
+    }
+    echo " - ";
 }
